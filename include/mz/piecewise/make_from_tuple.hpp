04566380a561bfd8b0f9a07f8f5ec9ac5be55f91
Zachary Michaels <mikezackles@gmail.com>
2016-07-27 11:36:43 -0400

    Allow for generic forwarding
    (e.g., to a factory function)

--- include/mz/piecewise/make_from_tuple.hpp
+++ include/mz/piecewise/make_from_tuple.hpp
@@ -7,10 +7,21 @@
 
 namespace mz { namespace piecewise {
   namespace detail {
-    template <typename T, typename ...Args, std::size_t ...Indices>
-    T make_from_tuple_and_indices(std::tuple<Args...> const& args, std::index_sequence<Indices...>) {
-      return T{std::forward<Args>(std::get<Indices>(args))...};
+    template <typename ...Args, std::size_t ...Indices, typename Callback>
+    auto unpack_tuple(
+      std::tuple<Args...> const& args
+    , std::index_sequence<Indices...>
+    , Callback&& callback) {
+      return callback(std::forward<Args>(std::get<Indices>(args))...);
     }
+  }
+
+  // This function can be used for generic forwarding (e.g., to a factory function)
+  template <typename ...Args, typename Callback>
+  auto forward_tuple(std::tuple<Args...> const& args, Callback&& callback) {
+    return detail::unpack_tuple(
+      args, std::make_index_sequence<sizeof...(Args)>{}, std::forward<Callback>(callback)
+    );
   }
 
   // Use this in conjunction with std::piecewise_construct,
@@ -18,10 +29,8 @@ namespace mz { namespace piecewise {
   // forwarding to construct multiple data members. See README.md for more
   // detail.
   template <typename T, typename ...Args>
-  T make_from_tuple(std::tuple<Args...> const& args) noexcept(std::is_nothrow_constructible<T, Args...>::value) {
-    return detail::make_from_tuple_and_indices<T>(
-      args, std::make_index_sequence<sizeof...(Args)>{}
-    );
+  T make_from_tuple(std::tuple<Args...> const& packed) noexcept(std::is_nothrow_constructible<T, Args...>::value) {
+    return forward_tuple(packed, [](auto&&... args) { return T{std::forward<decltype(args)>(args)...}; });
   }
 }}
 
