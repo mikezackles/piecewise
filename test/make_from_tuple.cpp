645704202d0afd279cab6bc368bc2c8540ecc1dc
Zachary Michaels <mikezackles@gmail.com>
2017-02-20 22:09:23 -0800

    Tweak Forwarder interface

--- test/make_from_tuple.cpp
+++ test/make_from_tuple.cpp
@@ -2,46 +2,77 @@
 #include <mz/piecewise/make_from_tuple.hpp>
 
 namespace {
-  template <typename T>
-  class Factory {
-  private:
-    struct VeryPrivate{};
+  template <typename T, typename Private>
+  class BracedConstructor {
+  public:
+    template <typename ...Args>
+    T operator()(Args&&... args) {
+      return T{Private{}, std::forward<Args>(args)...};
+    }
+  };
+
+  template <typename T, typename Private>
+  class RegularConstructor {
+  public:
+    template <typename ...Args>
+    T operator()(Args&&... args) {
+      return T(Private{}, std::forward<Args>(args)...);
+    }
+  };
+
+  template <typename T, typename Private>
+  class FactoryFunction {
+    template <typename ...Args>
+    T operator()(Args&&... args) {
+      return T::create(Private{}, std::forward<Args>(args)...);
+    }
+  };
+
+  template <typename T, template <typename, typename> class Unwrapper>
+  class Forwarder {
+  protected:
+    struct Private{};
 
   public:
     template <typename ...RefTypes>
-    struct Wrapper {
-      Wrapper(VeryPrivate, std::tuple<RefTypes...>&& packed_args_)
-      : packed_args{std::move(packed_args_)} {}
+    class Wrapper {
+    public:
+      Wrapper(std::tuple<RefTypes...> packed_args_)
+        : packed_args{std::move(packed_args_)}
+      {}
 
+      T create() {
+        return mz::piecewise::forward_tuple(
+          std::move(packed_args)
+        , Unwrapper<T, Private>{}
+        );
+      }
+
+    private:
       std::tuple<RefTypes...> packed_args;
     };
 
     template <typename ...Args>
-    static Wrapper<Args&&...> wrap(Args&&... args) {
-      return {VeryPrivate{}, std::forward_as_tuple(std::forward<Args>(args)...)};
+    static Wrapper<Args&&...> forward(Args&&... args) {
+      return {std::forward_as_tuple(std::forward<Args>(args)...)};
     }
 
-    template <typename ...RefTypes>
-    static T unwrap(Wrapper<RefTypes...>&& wrapped) {
-      return mz::piecewise::forward_tuple(
-        std::move(wrapped.packed_args)
-      , [](auto&&... args) { return T{Private{}, std::forward<decltype(args)>(args)...}; }
-      );
+    template <typename ...Args>
+    static T create(Args&&... args) {
+      return forward(std::forward<Args>(args)...).create();
     }
-
-  protected:
-    struct Private{};
   };
 
-  struct A : public Factory<A> {
+  struct A : public Forwarder<A, BracedConstructor> {
     A(Private, std::string foo_, int thirty_three_)
-    : foo{std::move(foo_)}, thirty_three{thirty_three_} {}
+      : foo{std::move(foo_)}, thirty_three{thirty_three_}
+    {}
 
     std::string foo;
     int thirty_three;
   };
 
-  struct B : public Factory<B> {
+  struct B : public Forwarder<B, BracedConstructor> {
     B(Private, int forty_two_, std::string bar_, int seventy_seven_)
     : forty_two{forty_two_}, bar{std::move(bar_)}, seventy_seven{seventy_seven_} {}
 
@@ -54,12 +85,8 @@ namespace {
   class Aggregate {
   public:
     template <typename TArgs, typename UArgs>
-    Aggregate(
-      TArgs t_args
-    , UArgs u_args
-    )
-      : t{T::unwrap(std::move(t_args))}
-      , u{U::unwrap(std::move(u_args))}
+    Aggregate(TArgs t_args, UArgs u_args)
+      : t{t_args.create()}, u{u_args.create()}
     {}
 
     T t;
@@ -70,8 +97,8 @@ namespace {
 SCENARIO("piecewise construction") {
   GIVEN("an aggregate type constructed with rvalue tuples") {
     Aggregate<A, B> aggregate{
-      A::wrap("foo", 33)
-    , B::wrap(42, "bar", 77)
+      A::forward("foo", 33)
+    , B::forward(42, "bar", 77)
     };
 
     THEN("piecewise construction works") {
