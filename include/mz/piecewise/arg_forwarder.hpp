d2fea66b8616a4fd9a78ea641e64c955e77c8a27
Zachary Michaels <mikezackles@gmail.com>
2017-04-23 13:23:55 -0700

    Extract multifail

--- include/mz/piecewise/arg_forwarder.hpp
+++ include/mz/piecewise/arg_forwarder.hpp
@@ -2,7 +2,6 @@
 #define UUID_49150B38_5CFC_48B8_91E5_4A965B99305D
 
 #include <mz/piecewise/make_from_tuple.hpp>
-#include <mz/piecewise/tuple_list.hpp>
 
 namespace mz { namespace piecewise {
   template <typename Callback, typename ...RefTypes>
@@ -40,55 +39,6 @@ namespace mz { namespace piecewise {
     return make_wrapper(
       std::forward_as_tuple(std::forward<Args>(args)...)
     , std::forward<Callback>(callback)
-    );
-  }
-
-  namespace detail {
-    template <
-      typename ...ArgPacks, typename ...Thunks
-    , typename OnSuccess, typename OnFail
-    > static auto multifail_impl(
-      std::tuple<ArgPacks...> arg_packs, std::tuple<Thunks...> thunks
-    , OnSuccess& on_success, OnFail& on_fail
-    ) {
-      auto split_arg_packs = tuple_list::split(std::move(arg_packs));
-      return split_arg_packs.unpack_head().construct(
-        [ arg_packs = std::move(split_arg_packs.tail)
-        , thunks = std::move(thunks)
-        , &on_success, &on_fail
-        ] (auto thunk) {
-          multifail(
-            std::move(arg_packs)
-          , tuple_list::combine(thunk, thunks)
-          , on_success, on_fail
-          );
-        }
-      , on_fail
-      );
-    }
-
-    template <
-      typename ...Thunks
-    , typename OnSuccess, typename OnFail
-    > static auto multifail_impl(
-      std::tuple<>, std::tuple<Thunks...> thunks
-    , OnSuccess& on_success, OnFail&
-    ) {
-      return forward_tuple(std::move(thunks), on_success);
-    }
-  }
-
-  template <
-    typename ...ArgPacks
-  , typename OnSuccess, typename OnFail
-  > static auto multifail(
-    OnSuccess&& on_success, OnFail&& on_fail
-  , ArgPacks&&... arg_packs
-  ) {
-    return detail::multifail_impl(
-      std::forward_as_tuple(std::forward<ArgPacks>(arg_packs)...)
-    , std::tuple<>{}
-    , on_success, on_fail
     );
   }
 }}
---
+++ include/mz/piecewise/multifail.hpp
@@ -0,0 +1,58 @@
+#ifndef UUID_5562D1DE_F4CC_4547_8BC3_0D931B26520D
+#define UUID_5562D1DE_F4CC_4547_8BC3_0D931B26520D
+
+#include <mz/piecewise/make_from_tuple.hpp>
+#include <mz/piecewise/tuple_list.hpp>
+
+namespace mz { namespace piecewise {
+  namespace detail {
+    template <
+      typename ...ArgPacks, typename ...Thunks
+    , typename OnSuccess, typename OnFail
+    > static auto multifail_impl(
+      std::tuple<ArgPacks...> arg_packs, std::tuple<Thunks...> thunks
+    , OnSuccess& on_success, OnFail& on_fail
+    ) {
+      auto split_arg_packs = tuple_list::split(std::move(arg_packs));
+      return split_arg_packs.unpack_head().construct(
+        [ arg_packs = std::move(split_arg_packs.tail)
+        , thunks = std::move(thunks)
+        , &on_success, &on_fail
+        ] (auto thunk) {
+          multifail(
+            std::move(arg_packs)
+          , tuple_list::combine(thunk, thunks)
+          , on_success, on_fail
+          );
+        }
+      , on_fail
+      );
+    }
+
+    template <
+      typename ...Thunks
+    , typename OnSuccess, typename OnFail
+    > static auto multifail_impl(
+      std::tuple<>, std::tuple<Thunks...> thunks
+    , OnSuccess& on_success, OnFail&
+    ) {
+      return forward_tuple(std::move(thunks), on_success);
+    }
+  }
+
+  template <
+    typename ...ArgPacks
+  , typename OnSuccess, typename OnFail
+  > static auto multifail(
+    OnSuccess&& on_success, OnFail&& on_fail
+  , ArgPacks&&... arg_packs
+  ) {
+    return detail::multifail_impl(
+      std::forward_as_tuple(std::forward<ArgPacks>(arg_packs)...)
+    , std::tuple<>{}
+    , on_success, on_fail
+    );
+  }
+}}
+
+#endif
