94591f0452cf2a277016deba0884c6a75baf368d
Zachary Michaels <mikezackles@gmail.com>
2017-04-30 19:12:09 -0700

    Add piecewise.hpp

---
+++ include/mz/piecewise.hpp
@@ -0,0 +1,5 @@
+#include <mz/piecewise/arg_forwarder.hpp>
+#include <mz/piecewise/forward_tuple.hpp>
+#include <mz/piecewise/lambda_overload.hpp>
+#include <mz/piecewise/multifail.hpp>
+#include <mz/piecewise/tuple_list.hpp>
