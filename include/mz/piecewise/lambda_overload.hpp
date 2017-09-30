4fac181b2f44a145c70e517efaa082cc0f653df5
Zachary Michaels <mikezackles@gmail.com>
2017-09-30 04:53:45 -0700

    Perfect forwarding for overloads along with variadic using for C++17

--- include/mz/piecewise/lambda_overload.hpp
+++ include/mz/piecewise/lambda_overload.hpp
@@ -1,13 +1,24 @@
 #ifndef UUID_AA9FDD71_5F9D_4874_B5E2_888E951DB878
 #define UUID_AA9FDD71_5F9D_4874_B5E2_888E951DB878
 
+#include <type_traits>
+
 namespace mz { namespace piecewise {
+#if __cplusplus >= 201703L
+  template <typename ...Lambdas>
+  struct LambdaOverload : Lambdas... {
+    using Lambdas::operator()...;
+  };
+#else
   template <typename Lambda, typename ...Lambdas>
   struct LambdaOverload : Lambda, LambdaOverload<Lambdas...> {
     using Lambda::operator();
     using LambdaOverload<Lambdas...>::operator();
-    LambdaOverload(Lambda lambda, Lambdas... lambdas)
-      : Lambda{std::move(lambda)}, LambdaOverload<Lambdas...>{std::move(lambdas)...} {}
+    template <typename ForwardLambda, typename ...ForwardLambdas>
+    LambdaOverload(ForwardLambda&& lambda, ForwardLambdas&&... lambdas)
+      : Lambda{std::forward<ForwardLambda>(lambda)}
+      , LambdaOverload<Lambdas...>{std::forward<ForwardLambdas>(lambdas)...}
+    {}
   };
 
   template <typename Lambda>
@@ -15,10 +26,13 @@ namespace mz { namespace piecewise {
     using Lambda::operator();
     LambdaOverload(Lambda lambda) : Lambda{std::move(lambda)} {}
   };
+#endif
 
-  template <typename ...Lambdas>
-  auto handler(Lambdas... lambdas) {
-    return LambdaOverload<Lambdas...>{std::move(lambdas)...};
+  template <typename ...ForwardLambdas>
+  auto handler(ForwardLambdas&&... lambdas) {
+    return LambdaOverload<std::remove_reference_t<ForwardLambdas>...>{
+      std::forward<ForwardLambdas>(lambdas)...
+    };
   }
 }}
 
